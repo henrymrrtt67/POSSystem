d22790675f633af10f9fb3ba3e9cecce2c85ad77
merrithenr <henrymrrtt1@gmail.com>
2019-02-08 13:01:44 +1300

    comments and new function

--- POS.py
+++ POS.py
@@ -5,25 +5,43 @@ Created on Thu Feb  7 14:36:55 2019
 @author: Henry
 """
 
+#start of the program
 def start():
+    #Categories for those 
     categories = {1:"Wine",
                   2:"Beer",
                   3:"Spirits",
                   4:"Food"}
+    #program on an infinite loop
     while(0):
         item = catSelection(categories)
-        
+
+#allows the user to select the categories        
 def catSelection(categories):
     selection = 0
+    print(categories)
     print("Input your selection.")
+    #Loop until a category has been selected
     while(selection==0):
         userInput = input()
         userInput = int(userInput)
-            if(userInput-1<0 && userInput-1>3):
-                print("That input does not exist; please try again.")
-            else:
-                items(userInput, categories)
-                
+            #if the user input is out of bounds
+        if(userInput-1>0):
+            print("That input does not exist; please try again.")
+            #if the items are correct it goes through else
+        else:
+            genItems(userInput, categories)
+
+#splitting between the categories available.                
 def genItems(userInput, categories):
-    for(i in categories):
-        if(i == userInput):
+    #checking each category for the right one
+    for cat in categories:
+        if(cat == userInput):
+            if(cat == "Wine"):
+                wineSele()
+            if(cat == "Beer"):
+                beerSele()
+            if(cat == "Spirits"):
+                spiritSele()
+            if(cat == "Food"):
+                foodSele()
